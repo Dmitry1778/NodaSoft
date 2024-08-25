4a2f0b2c489b052f780cec1f622ff311d8db492f
Dmitry <diman17781@gmail.com>
2024-08-25 19:54:48 +0300

    add comments

--- cmd/main.go
+++ cmd/main.go
@@ -16,81 +16,84 @@ import (
 // Как видите, никаких привязок к внешним сервисам нет - полный карт-бланш на модификацию кода.
 
 func main() {
-	superChan := make(chan Ttype, 10)
+	superChan := make(chan Ttype, 10) // Канал для создания тасков
 
-	go createTask(superChan)
+	go createTask(superChan) // Запускаем горутину для генерации тасков
 
-	doneTasks := make(chan Ttype)
+	doneTasks := make(chan Ttype) // Канал для завершенных тасков
 
-	undoneTasks := make(chan error)
+	undoneTasks := make(chan error) // Канал для ошибок
 
-	go taskWorker(superChan, doneTasks, undoneTasks)
+	go taskWorker(superChan, doneTasks, undoneTasks) // Запускаем горутину для обработки тасков
 
-	go getTask(doneTasks, undoneTasks)
+	go getTask(doneTasks, undoneTasks) // Запускаем горутину для вывода результатов
 
 	time.Sleep(10 * time.Second)
 
-	close(superChan)
+	close(superChan) //Закрытие канала для создания тасков
 
 	time.Sleep(3 * time.Second)
 
 }
 
-func createTask(a chan Ttype) {
+// Методот генерации тасков
+func createTask(taskChan chan Ttype) {
 	for {
 		select {
-		case <-time.After(3 * time.Second):
+		case <-time.After(3 * time.Second): // Каждые 3 секунды генерации
 			ft := time.Now().Format(time.RFC3339)
 			if time.Now().Nanosecond()%2 > 0 { // вот такое условие появления ошибочных тасков
 				ft = "Some error occured"
 			}
-			a <- Ttype{cT: ft, id: int(time.Now().Unix())} // передаем таск на выполнение
-		case <-a:
+			taskChan <- Ttype{cT: ft, id: int(time.Now().Unix())} // передаем таск на выполнение
+		case <-taskChan:
 			return
 		}
 	}
 }
 
+// Метод обработки тасков
 func taskWorker(taskChan chan Ttype, tasks chan Ttype, undoneTasks chan error) {
 	for task := range taskChan {
 		go func(t Ttype) {
-			tt, _ := time.Parse(time.RFC3339, t.cT)
-			if tt.After(time.Now().Add(-20 * time.Second)) {
-				t.taskRESULT = []byte("task has been successed")
+			tt, _ := time.Parse(time.RFC3339, t.cT)          // Извлекаем время из t.cT
+			if tt.After(time.Now().Add(-20 * time.Second)) { // Проверяем, было ли время создания задачи менее 20 секунд назад
+				t.taskRESULT = []byte("task has been successed") // Если да то записываем такое результат
 			} else {
-				t.taskRESULT = []byte("something went wrong")
+				t.taskRESULT = []byte("something went wrong") // Иначе такое
 			}
-			t.fT = time.Now().Format(time.RFC3339Nano)
+			t.fT = time.Now().Format(time.RFC3339Nano) // В поле t.fT записывается текущее время завершения задачи
 
-			time.Sleep(time.Millisecond * 150)
+			time.Sleep(time.Millisecond * 150) // Искусственная задержка в 150 миллисекунд для имитации обработки задачи
 
-			if string(t.taskRESULT[14:]) == "successed" {
-				tasks <- t
+			if string(t.taskRESULT[14:]) == "successed" { // Проверка результата taskResult
+				tasks <- t // записываем в tasks
 			} else {
-				undoneTasks <- fmt.Errorf("Task id %d time %s, error %s", t.id, t.cT, t.taskRESULT)
+				undoneTasks <- fmt.Errorf("Task id %d time %s, error %s", t.id, t.cT, t.taskRESULT) // Иначе записываем ошибку
 			}
-		}(task)
+		}(task) // Пробегаемся по всем таскам через анонимную функцию. Количество пробежки зависит от количества tasks
 	}
 }
 
+// Метод получения tasks
 func getTask(doneTasks chan Ttype, undoneTasks chan error) {
-	for {
-		select {
+	for { // Бесконечный цикл выполняется пока не функция не завершится
+		select { // ожидает, пока один из каналов будет готов
 		case task, ok := <-doneTasks:
 			if ok {
-				fmt.Printf("Done task: ID: %d, Time: %sn", task.id, task.cT)
+				fmt.Printf("Done task: ID: %d, Time: %sn", task.id, task.cT) // Если есть таск выводим это
 			} else {
 				fmt.Println("Done tasks channel closed")
 				return
 			}
 		case err, ok := <-undoneTasks:
 			if ok {
-				fmt.Printf("Error: %vn", err)
+				fmt.Printf("Error: %vn", err) // Если есть нет таск выводим это
 			} else {
 				fmt.Println("Undone tasks channel closed")
 				return
 			}
-		case <-time.After(3 * time.Second):
+		case <-time.After(3 * time.Second): // Если ни один из каналов не был готов в течение 3 секунд, выводится строка "----------"
 			fmt.Println("----------")
 		}
 	}
